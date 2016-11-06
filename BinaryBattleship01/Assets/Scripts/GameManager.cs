532241476380b5b9f2dc9639be87e3ac34cb3145
JonWong1661 <Jon@Jons-Air.home>
2016-11-05 23:41:34 -0400

    Menu

---
+++ BinaryBattleship01/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public void OnSartGame(string sceneName)
+	{
+		Application.LoadLevel (sceneName);
+	}
+
+}
