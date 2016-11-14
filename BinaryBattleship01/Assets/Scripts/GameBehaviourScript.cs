0fac1b200046fb4cbf79de56e9ab1ae39ee6fc6a
kast114 <kast114@gmail.com>
2016-11-13 22:25:12 -0500

    Changes to Scan
    Scan button reveals the image behind it
    There is also a scan button that is a child of the panel but using it
    makes the enitre panel and button dissappear.

--- BinaryBattleship01/Assets/Scripts/GameBehaviourScript.cs
+++ BinaryBattleship01/Assets/Scripts/GameBehaviourScript.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class GameBehaviourScript : MonoBehaviour {
+	public Panel obj;
 
 	public void OnScan(){
 		Scene scene = SceneManager.GetActiveScene ();
 		if (scene.name == "02 - Game")
 			Debug.Log ("You did it");
+		    
+		//obj.TogglePanel(GameObject panel);
 	}
 }
