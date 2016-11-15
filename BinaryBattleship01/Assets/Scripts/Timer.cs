bcbee44efa02e952bcccb29492e0366f2616ba33
AdrianLl <AdrianLl@users.noreply.github.com>
2016-11-14 23:08:29 -0500

    added timer for countdown when the game starts

---
+++ BinaryBattleship01/Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float countdownTimer  = 92;
+    public Text timerText;
+
+
+	// Use this for initialization
+	void Start () {
+
+        timerText = GetComponent<Text>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        countdownTimer -= Time.deltaTime;
+        timerText.text = countdownTimer.ToString("f0");
+        print(countdownTimer);
+	
+	}
+}
