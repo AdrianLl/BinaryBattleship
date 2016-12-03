4f197cd08674750679737574970a026bd6eb1578
Adrian  Lliguichuzhca <09adrianll@gmail.com>
2016-12-03 10:14:21 -0500

    added ship spawn on start

---
+++ BinaryBattleship01/Assets/Scripts/ShipSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipSpawner : MonoBehaviour {
+
+    public int maxShips;
+    public GameObject ship;
+    public float[] horizontalLocation;
+    public float[] verticalLocation;
+    private GameStates gameStates;
+
+
+    void Start()
+    {
+        maxShips = 4;
+
+        horizontalLocation = new float[4] { -3.13f, -2.17f, 2.92f, 4.87f };
+        verticalLocation = new float[4] { -1.72f, 3.25f, -0.72f, 1.26f };
+
+
+    }
+
+    public void spawnShips()
+    {
+        gameStates = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameStates>();
+
+        if (gameStates.gameON)
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        for (int i = 0; i < maxShips; i++)
+        {
+            Vector2 position = new Vector2(horizontalLocation[i], verticalLocation[i]);
+            Instantiate(ship, position, Quaternion.identity);
+        }
+
+    }
+}
